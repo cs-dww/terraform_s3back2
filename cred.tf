82ffc6754cf9d0b40c8e04519f0bae7faaabb19e
root <root@ip-172-31-42-24.us-west-2.compute.internal>
2021-12-12 00:21:36 +0000

    adding temp creds file for Jenkins build

---
+++ cred.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  access_key = "ASIASYYEA7DMFXZQXKGQ"
+  secret_key = "clvQ65XVJnO2vOyqkJJf+A5LIPQ4NH1Qp5fTilgk"
+  token = "FwoGZXIvYXdzEEkaDNjgB0326s1wu88mRiK6Act0lDUmaeFEPzwzQQeBugw/dN8cPM89kcwgE4/QYQzbb8qWWDynnTc+WKnR9go8fYM0nIo0neXfiZuQ9UB/17c3osl7Qm78qyWbbEV+vJDAwFzcgJ3E+GBvk9FIARDAHXOKdAsdGbuVAIlsq2JVW/DSxVG5oROLGhThMd5+2m/i7lbmow/Zd0G6P3AmnUQ/3UeOexVvEbfc36iRQewvrzTJp3oyqJln4F4yHsDl3kX3pO1KzPHThurjaCjv3tSNBjItAoeqKPPZzZyEdbNOBpZAk3G9uPlkehw501foHSkbd7cP/+SRuJg0ge9SZBVk"
+  region = "us-east-1"
+}
